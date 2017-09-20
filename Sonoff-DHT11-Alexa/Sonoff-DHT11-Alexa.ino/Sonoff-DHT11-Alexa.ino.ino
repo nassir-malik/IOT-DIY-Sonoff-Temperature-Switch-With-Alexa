4989ba037ddfcc3dddcde6f7b717b5a8ce2d451a
nassiramalik <nassir.malik@gmail.com>
2017-09-19 21:07:01 -0500

    new

--- Sonoff-DHT11-Alexa/Sonoff-DHT11-Alexa.ino/Sonoff-DHT11-Alexa.ino.ino
+++ Sonoff-DHT11-Alexa/Sonoff-DHT11-Alexa.ino/Sonoff-DHT11-Alexa.ino.ino
@@ -11,12 +11,12 @@
 #include <WebSocketsClient.h>
 #include <Hash.h>
 // @@@@@@@@@@@@@@@ You only need to modify wi-fi and domain info @@@@@@@@@@@@@@@@@@@@
-const char* ssid     = "enter your ssid"; //enter your ssid/ wi-fi(case sensitive) router name - 2.4 Ghz only
-const char* password = "enter ssid password";     // enter ssid password (case sensitive)
+const char* ssid     = "suddenlink.net-AD42"; //enter your ssid/ wi-fi(case sensitive) router name - 2.4 Ghz only
+const char* password = "G7MBSY89C601814";     // enter ssid password (case sensitive)
 char host[] = "smarttempswitch.herokuapp.com"; //- better your Heroku domain name like  "smarttempswitch.herokuapp.com" 
 // @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-#define dht_dpin 14
-const int relayPin = 16;
+#define dht_dpin 14 //Sonoff
+const int relayPin = 12; //Sonoff relay
 // @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 dht DHT;
 int port = 80;
